f478bef02552e8ea9d6c73fa1e9e05509bd09d77
oirien <robert.mayo.ch@gmail.com>
2023-10-05 14:10:30 +0100

    Initial Commit

---
+++ src/main/java/com/ByteStorm/ByteStormService/ByteStormServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ByteStorm.ByteStormService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteStormServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteStormServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ByteStorm/ByteStormService/ByteStormServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ByteStorm.ByteStormService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteStormServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
