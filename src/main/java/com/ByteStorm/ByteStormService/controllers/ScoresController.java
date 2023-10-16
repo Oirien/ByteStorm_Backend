868443e55b5d0c131a56da5c327a104aba27f749
oirien <robert.mayo.ch@gmail.com>
2023-10-16 16:07:49 +0100

    added paginated query to backend service

--- src/main/java/com/ByteStorm/ByteStormService/controllers/ScoresController.java
+++ src/main/java/com/ByteStorm/ByteStormService/controllers/ScoresController.java
@@ -3,12 +3,12 @@ package com.ByteStorm.ByteStormService.controllers;
 import com.ByteStorm.ByteStormService.models.Scores;
 import com.ByteStorm.ByteStormService.repositories.ScoresRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,10 +17,14 @@ public class ScoresController {
     @Autowired
     ScoresRepository scoresRepository;
 
-    @GetMapping(value="/api/leaderboard")
-    public ResponseEntity<List<Scores>> getScores() {
-        List<Scores> scores = scoresRepository.findByOrderByScoreDesc();
-        return new ResponseEntity<>(scores, HttpStatus.OK);
+    @GetMapping(value = "/api/leaderboard")
+    public ResponseEntity<Page<Scores>> getScores(
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "pageSize", defaultValue = "10") int pageSize
+    ) {
+        PageRequest pageRequest = PageRequest.of(page, pageSize, Sort.by(Sort.Direction.DESC, "score"));
+        Page<Scores> scoresPage = scoresRepository.findAll(pageRequest);
+        return new ResponseEntity<>(scoresPage, HttpStatus.OK);
     }
 
     @PostMapping(value="/api/leaderboard")
